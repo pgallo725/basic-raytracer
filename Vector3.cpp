445dacb7ea24ba10198a0a731715b35c90c46ddb
Paolo Gallo <s257428@studenti.polito.it>
2021-08-22 18:55:00 +0200

    Move all of Vector3's code into a dedicated Vector3.cpp file

---
+++ Vector3.cpp
@@ -0,0 +1,132 @@
+#include "Vector3.h"
+
+
+Vector3 Vector3::operator-() const
+{
+	return Vector3(-values[0], -values[1], -values[2]);
+}
+
+Vector3& Vector3::operator+=(const Vector3& other)
+{
+	values[0] += other.values[0];
+	values[1] += other.values[1];
+	values[2] += other.values[2];
+	return *this;
+}
+
+Vector3& Vector3::operator*=(const double value)
+{
+	values[0] *= value;
+	values[1] *= value;
+	values[2] *= value;
+	return *this;
+}
+
+Vector3& Vector3::operator/=(const double value)
+{
+	return *this *= 1 / value;
+}
+
+Vector3 Vector3::operator+(const Vector3& other) const
+{
+	return Vector3(this->values[0] + other.values[0],
+		this->values[1] + other.values[1],
+		this->values[2] + other.values[2]);
+}
+
+Vector3 Vector3::operator-(const Vector3& other) const
+{
+	return Vector3(this->values[0] - other.values[0],
+		this->values[1] - other.values[1],
+		this->values[2] - other.values[2]);
+}
+
+Vector3 Vector3::operator*(const Vector3& other) const
+{
+	return Vector3(this->values[0] * other.values[0],
+		this->values[1] * other.values[1],
+		this->values[2] * other.values[2]);
+}
+
+Vector3 Vector3::operator*(const double val) const
+{
+	return Vector3(this->values[0] * val,
+		this->values[1] * val,
+		this->values[2] * val);
+}
+
+Vector3 Vector3::operator/(const double val) const
+{
+	return *this * (1 / val);
+}
+
+
+double Vector3::Length() const
+{
+	return std::sqrt(SqrLength());
+}
+
+double Vector3::SqrLength() const
+{
+	return values[0] * values[0] + values[1] * values[1] + values[2] * values[2];
+}
+
+bool Vector3::NearZero() const
+{
+	// Return true if the vector is close to zero in all dimensions.
+	const auto eps = 1e-8;
+	return (fabs(values[0]) < eps) && (fabs(values[1]) < eps) && (fabs(values[2]) < eps);
+}
+
+
+std::ostream& operator<<(std::ostream& out, const Vector3& vec)
+{
+	return out << vec.values[0] << ' ' << vec.values[1] << ' ' << vec.values[2];
+}
+
+Vector3 operator*(const double val, const Vector3& vec)
+{
+	return vec * val;
+}
+
+
+double Vector3::Dot(const Vector3& a, const Vector3& b)
+{
+	return a.values[0] * b.values[0] +
+		a.values[1] * b.values[1] +
+		a.values[2] * b.values[2];
+}
+
+Vector3 Vector3::Cross(const Vector3& a, const Vector3& b)
+{
+	return Vector3(a.values[1] * b.values[2] - a.values[2] * b.values[1],
+		a.values[2] * b.values[0] - a.values[0] * b.values[2],
+		a.values[0] * b.values[1] - a.values[1] * b.values[0]);
+}
+
+Vector3 Vector3::Normalized(const Vector3& vec)
+{
+	return vec / vec.Length();
+}
+
+Vector3 Vector3::Reflect(const Vector3& vec, const Vector3& normal)
+{
+	// r = v - 2 * (v.n) * n
+	return vec - 2 * Vector3::Dot(vec, normal) * normal;
+}
+
+Vector3 Vector3::Refract(const Vector3& vec, const Vector3& normal, double etai_over_etat)
+{
+	// Splitting the refracted ray into a R'_perpendicular and R'_parallel,
+	// using Snell's law we derive that R'_perp = etai/etat * (R + cos(theta)*n)
+	// by exploiting the definition of dot product and restricting the vectors
+	// to be of unit length, it can be written as R'_perp = etai/etat * (R + (-R.n)*n)
+	double cos_theta = fmin(Vector3::Dot(-vec, normal), 1.0);
+
+	Vector3 r_perpendicular = etai_over_etat * (vec + cos_theta * normal);
+
+	// We also derive that R'_parallel = -sqrt(1 - |R'_perp|^2) * n
+	Vector3 r_parallel = -std::sqrt(fabs(1.0 - r_perpendicular.SqrLength())) * normal;
+
+	return r_parallel + r_perpendicular;
+}
--- Vector3.h
+++ Vector3.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <iostream>
 
+
 class Vector3 
 {
 public:
@@ -18,129 +19,39 @@ public:
 	inline double y() { return values[1]; }
 	inline double z() { return values[2]; }
 
-	inline Vector3 operator-() const { return Vector3(-values[0], -values[1], -values[2]); }
 	inline double operator[](int i) const { return values[i]; }
 	inline double& operator[](int i) { return values[i]; }
 
-	inline Vector3& operator+=(const Vector3& other)
-	{
-		values[0] += other.values[0];
-		values[1] += other.values[1];
-		values[2] += other.values[2];
-		return *this;
-	}
+	Vector3 operator-() const;
 
-	inline Vector3& operator*=(const double value)
-	{
-		values[0] *= value;
-		values[1] *= value;
-		values[2] *= value;
-		return *this;
-	}
+	Vector3& operator+=(const Vector3& other);
+	Vector3& operator*=(const double value);
+	Vector3& operator/=(const double value);
 
-	inline Vector3& operator/=(const double value)
-	{
-		return *this *= 1 / value;
-	}
+	Vector3 operator+(const Vector3& other) const;
+	Vector3 operator-(const Vector3& other) const;
+	Vector3 operator*(const Vector3& other) const;
+	Vector3 operator*(const double val) const;
+	Vector3 operator/(const double val) const;
 
-	inline Vector3 operator+(const Vector3& other) const
-	{
-		return Vector3(this->values[0] + other.values[0],
-			this->values[1] + other.values[1],
-			this->values[2] + other.values[2]);
-	}
+	double Length() const;
+	double SqrLength() const;
 
-	inline Vector3 operator-(const Vector3& other) const
-	{
-		return Vector3(this->values[0] - other.values[0],
-			this->values[1] - other.values[1],
-			this->values[2] - other.values[2]);
-	}
+	bool NearZero() const;
 
-	inline Vector3 operator*(const Vector3& other) const
-	{
-		return Vector3(this->values[0] * other.values[0],
-			this->values[1] * other.values[1],
-			this->values[2] * other.values[2]);
-	}
+	static Vector3 Normalized(const Vector3& vec);
 
-	inline Vector3 operator*(const double val) const
-	{
-		return Vector3(this->values[0] * val,
-			this->values[1] * val,
-			this->values[2] * val);
-	}
+	static double Dot(const Vector3& a, const Vector3& b);
+	static Vector3 Cross(const Vector3& a, const Vector3& b);
 
-	inline Vector3 operator/(const double val) const
-	{
-		return *this * (1 / val);
-	}
-
-
-	inline double Length() const { return std::sqrt(SqrLength()); }
-	inline double SqrLength() const { return values[0] * values[0] + values[1] * values[1] + values[2] * values[2]; }
-
-	inline bool NearZero() const
-	{
-		// Return true if the vector is close to zero in all dimensions.
-		const auto eps = 1e-8;
-		return (fabs(values[0]) < eps) && (fabs(values[1]) < eps) && (fabs(values[2]) < eps);
-	}
-
-
-	inline static double Dot(const Vector3& a, const Vector3& b)
-	{
-		return a.values[0] * b.values[0] +
-			a.values[1] * b.values[1] +
-			a.values[2] * b.values[2];
-	}
-
-	inline static Vector3 Cross(const Vector3& a, const Vector3& b)
-	{
-		return Vector3(a.values[1] * b.values[2] - a.values[2] * b.values[1],
-			a.values[2] * b.values[0] - a.values[0] * b.values[2],
-			a.values[0] * b.values[1] - a.values[1] * b.values[0]);
-	}
-
-	inline static Vector3 Normalized(const Vector3& vec)
-	{
-		return vec / vec.Length();
-	}
-
-	inline static Vector3 Reflect(const Vector3& vec, const Vector3& normal)
-	{
-		// Order of operands changed due to operator* shenanigans
-		// it should be r = v - 2 * (v.n) * n
-		return vec - normal * 2 * Vector3::Dot(vec, normal);
-	}
-
-	inline static Vector3 Refract(const Vector3& vec, const Vector3& normal, double etai_over_etat)
-	{
-		// Splitting the refracted ray into a R'_perpendicular and R'_parallel,
-		// using Snell's law we derive that R'_perp = etai/etat * (R + cos(theta)*n)
-		// by exploiting the definition of dot product and restricting the vectors
-		// to be of unit length, it can be written as R'_perp = etai/etat * (R + (-R.n)*n)
-		double cos_theta = fmin(Vector3::Dot(-vec, normal), 1.0);
-
-		Vector3 r_perpendicular = (vec + normal * cos_theta) * etai_over_etat;
-
-		// We also derive that R'_parallel = -sqrt(1 - |R'_perp|^2) * n
-		Vector3 r_parallel = normal * -std::sqrt(fabs(1.0 - r_perpendicular.SqrLength()));
-
-		return r_parallel + r_perpendicular;
-	}
+	static Vector3 Reflect(const Vector3& vec, const Vector3& normal);
+	static Vector3 Refract(const Vector3& vec, const Vector3& normal, double etai_over_etat);
 };
 
 
-inline std::ostream& operator<<(std::ostream& out, const Vector3& vec) 
-{
-	return out << vec.values[0] << ' ' << vec.values[1] << ' ' << vec.values[2];
-}
-
-inline Vector3 operator*(const double val, const Vector3& vec)
-{
-	return vec * val;
-}
+// Non-member Vector3 operators
+inline std::ostream& operator<<(std::ostream& out, const Vector3& vec);
+inline Vector3 operator*(const double val, const Vector3& vec);
 
 
 // Type aliases for Vector3
