d628247fff4e68ff7f74f56c6d728db1b768aff9
Paolo Gallo <s257428@studenti.polito.it>
2021-10-23 21:11:53 +0200

    Refactor Sphere class

--- src/Sphere.h
+++ src/Sphere.h
@@ -19,48 +19,48 @@ public:
     Sphere(Point3 center, double radius, std::shared_ptr<Material> material)
         : center(center), radius(radius), material(material) {}
 
-    virtual bool Hit(const Ray& ray, double t_min, double t_max, HitRecord& hit) const override;
-};
 
-
-bool Sphere::Hit(const Ray& ray, double t_min, double t_max, HitRecord& hit) const
-{
-    // Check if there exists any 't' that defines a point P which satisfies
-    // the equation (Px - Cx)^2 + (Py - Cy)^2 + (Pz - Cz)^2 = r^2,
-    // rewritten as ((O + t * d) - C).((O + t * d) - C) - r^2 = 0,
-    // where O is the ray origin and d is the ray direction,
-    // which defines a point on the sphere's surface (an intersection)
-    // The equation is unrolled as t^2 * d.d + + 2 * t * d.(O - C) + (O - C).(O - C) - r^2 = 0
-    // Solving for the unknown t, this means that:
-    //  a = |d|^2
-    //  b = 2 * d.(O - C)
-    //  c = |O - C|^2 - r^2
-    // Using h = d.(O - C) such as 2h = b it is possible to simplify the calculation of
-    // the discriminant and the solutions of the equation by removing some multiplications.
-    Vector3 oc = ray.origin - center;
-    double a = ray.direction.SqrLength();
-    double h = Vector3::Dot(oc, ray.direction);
-    double c = oc.SqrLength() - radius * radius;
-    double discriminant = h * h - a * c;
-
-    if (discriminant < 0) return false;
-    double sqrtd = std::sqrt(discriminant);
-
-    // Find the nearest root that lies in the specified range.
-    double root = (-h - sqrtd) / a;
-    if ((root < t_min) | (root > t_max))
+    // Ray-sphere intersection checking.
+    virtual bool Hit(const Ray& ray, const double t_min, const double t_max, HitRecord& hit) const noexcept override
     {
-        root = (-h + sqrtd) / a;
-        if ((root < t_min) | (t_max < root))
-            return false;
-    }
-    
-    hit.t = root;
-    hit.point = ray.At(hit.t);
-    Vector3 outward_normal = (hit.point - center) / radius;
-    hit.is_front_face = Vector3::Dot(ray.direction, outward_normal) < 0.0;
-    hit.normal = hit.is_front_face ? outward_normal : -outward_normal;
-    hit.material = material.get();
+        // Check if there exists any 't' that defines a point P which satisfies
+        // the equation (Px - Cx)^2 + (Py - Cy)^2 + (Pz - Cz)^2 = r^2,
+        // rewritten as ((O + t * d) - C).((O + t * d) - C) - r^2 = 0,
+        // where O is the ray origin and d is the ray direction,
+        // which defines a point on the sphere's surface (an intersection)
+        // The equation is unrolled as t^2 * d.d + + 2 * t * d.(O - C) + (O - C).(O - C) - r^2 = 0
+        // Solving for the unknown t, this means that:
+        //  a = |d|^2
+        //  b = 2 * d.(O - C)
+        //  c = |O - C|^2 - r^2
+        // Using h = d.(O - C) such as 2h = b it is possible to simplify the calculation of
+        // the discriminant and the solutions of the equation by removing some multiplications.
+        const Vector3 oc = ray.origin - center;
+        const double a = ray.direction.SqrLength();
+        const double h = Vector3::Dot(oc, ray.direction);
+        const double c = oc.SqrLength() - radius * radius;
+        const double discriminant = h * h - a * c;
 
-    return true;
-}
+        if (discriminant < 0) return false;
+        const double sqrtd = std::sqrt(discriminant);
+
+        // Find the nearest root that lies in the specified range.
+        double root = (-h - sqrtd) / a;
+        if ((root < t_min) | (root > t_max))
+        {
+            root = (-h + sqrtd) / a;
+            if ((root < t_min) | (t_max < root))
+                return false;
+        }
+
+        // Fill the HitRecord structure with all the info about the intersection.
+        hit.t = root;
+        hit.point = ray.At(hit.t);
+        const Vector3 outward_normal = (hit.point - center) / radius;
+        hit.is_front_face = Vector3::Dot(ray.direction, outward_normal) < 0.0;
+        hit.normal = hit.is_front_face ? outward_normal : -outward_normal;
+        hit.material = material.get();
+
+        return true;
+    }
+};
