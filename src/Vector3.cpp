c70c31dda006502f4c9d7a5542c8d718a2b34235
Paolo Gallo <s257428@studenti.polito.it>
2021-10-24 14:48:09 +0200

    Refactor Vector3 class

--- src/Vector3.cpp
+++ src/Vector3.cpp
@@ -2,12 +2,12 @@
 #include "Common.h"
 
 
-Vector3 Vector3::operator-() const
+Vector3 Vector3::operator-() const noexcept
 {
 	return Vector3(-values[0], -values[1], -values[2]);
 }
 
-Vector3& Vector3::operator+=(const Vector3& other)
+Vector3& Vector3::operator+=(const Vector3& other) noexcept
 {
 	values[0] += other.values[0];
 	values[1] += other.values[1];
@@ -15,7 +15,7 @@ Vector3& Vector3::operator+=(const Vector3& other)
 	return *this;
 }
 
-Vector3& Vector3::operator*=(const double value)
+Vector3& Vector3::operator*=(const double value) noexcept
 {
 	values[0] *= value;
 	values[1] *= value;
@@ -23,100 +23,92 @@ Vector3& Vector3::operator*=(const double value)
 	return *this;
 }
 
-Vector3& Vector3::operator/=(const double value)
+Vector3& Vector3::operator/=(const double value) noexcept
 {
-	return *this *= 1 / value;
+	return *this *= 1.0 / value;
 }
 
-Vector3 Vector3::operator+(const Vector3& other) const
+Vector3 Vector3::operator+(const Vector3& other) const noexcept
 {
 	return Vector3(this->values[0] + other.values[0],
 		this->values[1] + other.values[1],
 		this->values[2] + other.values[2]);
 }
 
-Vector3 Vector3::operator-(const Vector3& other) const
+Vector3 Vector3::operator-(const Vector3& other) const noexcept
 {
 	return Vector3(this->values[0] - other.values[0],
 		this->values[1] - other.values[1],
 		this->values[2] - other.values[2]);
 }
 
-Vector3 Vector3::operator*(const Vector3& other) const
+Vector3 Vector3::operator*(const Vector3& other) const noexcept
 {
 	return Vector3(this->values[0] * other.values[0],
 		this->values[1] * other.values[1],
 		this->values[2] * other.values[2]);
 }
 
-Vector3 Vector3::operator*(const double val) const
+Vector3 Vector3::operator*(const double val) const noexcept
 {
 	return Vector3(this->values[0] * val,
 		this->values[1] * val,
 		this->values[2] * val);
 }
 
-Vector3 Vector3::operator/(const double val) const
+Vector3 operator*(const double val, const Vector3& vec) noexcept
 {
-	return *this * (1 / val);
+	return vec * val;
 }
 
+Vector3 Vector3::operator/(const double val) const noexcept
+{
+	return *this * (1.0 / val);
+}
 
-double Vector3::Length() const
+double Vector3::Length() const noexcept
 {
 	return std::sqrt(SqrLength());
 }
 
-double Vector3::SqrLength() const
+double Vector3::SqrLength() const noexcept
 {
 	return values[0] * values[0] + values[1] * values[1] + values[2] * values[2];
 }
 
-bool Vector3::NearZero() const
+bool Vector3::NearZero() const noexcept
 {
 	// Return true if the vector is close to zero in all dimensions.
 	const auto eps = 1e-8;
 	return (fabs(values[0]) < eps) && (fabs(values[1]) < eps) && (fabs(values[2]) < eps);
 }
 
-
-std::ostream& operator<<(std::ostream& out, const Vector3& vec)
-{
-	return out << vec.values[0] << ' ' << vec.values[1] << ' ' << vec.values[2];
-}
-
-Vector3 operator*(const double val, const Vector3& vec)
-{
-	return vec * val;
-}
-
-
-double Vector3::Dot(const Vector3& a, const Vector3& b)
+double Vector3::Dot(const Vector3& a, const Vector3& b) noexcept
 {
 	return a.values[0] * b.values[0] +
 		a.values[1] * b.values[1] +
 		a.values[2] * b.values[2];
 }
 
-Vector3 Vector3::Cross(const Vector3& a, const Vector3& b)
+Vector3 Vector3::Cross(const Vector3& a, const Vector3& b) noexcept
 {
 	return Vector3(a.values[1] * b.values[2] - a.values[2] * b.values[1],
 		a.values[2] * b.values[0] - a.values[0] * b.values[2],
 		a.values[0] * b.values[1] - a.values[1] * b.values[0]);
 }
 
-Vector3 Vector3::Normalized(const Vector3& vec)
+Vector3 Vector3::Normalized(const Vector3& vec) noexcept
 {
 	return vec / vec.Length();
 }
 
-Vector3 Vector3::Reflect(const Vector3& vec, const Vector3& normal)
+Vector3 Vector3::Reflect(const Vector3& vec, const Vector3& normal) noexcept
 {
 	// r = v - 2 * (v.n) * n
 	return vec - 2 * Vector3::Dot(vec, normal) * normal;
 }
 
-Vector3 Vector3::Refract(const Vector3& vec, const Vector3& normal, double etai_over_etat)
+Vector3 Vector3::Refract(const Vector3& vec, const Vector3& normal, const double etai_over_etat) noexcept
 {
 	// Splitting the refracted ray into a R'_perpendicular and R'_parallel,
 	// using Snell's law we derive that R'_perp = etai/etat * (R + cos(theta)*n)
--- src/Vector3.h
+++ src/Vector3.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <cmath>
-#include <iostream>
+class Vector3;
+using Point3 = Vector3;			// 3D point
+using Color = Vector3;			// RGB color
 
 
 class Vector3 
@@ -12,48 +13,40 @@ public:
 
 public:
 
-	Vector3() : values{ 0, 0, 0 } {}
-	Vector3(double v1, double v2, double v3) : values{ v1, v2, v3 } {}
+	Vector3() noexcept : values{ 0, 0, 0 } {}
+	Vector3(double v1, double v2, double v3) noexcept : values{ v1, v2, v3 } {}
 
-	inline double x() const { return values[0]; }
-	inline double y() const { return values[1]; }
-	inline double z() const { return values[2]; }
+	double x() const noexcept { return values[0]; }
+	double y() const noexcept { return values[1]; }
+	double z() const noexcept { return values[2]; }
 
-	inline double operator[](int i) const { return values[i]; }
-	inline double& operator[](int i) { return values[i]; }
+	double operator[](const int i) const noexcept { return values[i]; }
+	double& operator[](const int i) noexcept { return values[i]; }
 
-	Vector3 operator-() const;
+	Vector3 operator-() const noexcept;
 
-	Vector3& operator+=(const Vector3& other);
-	Vector3& operator*=(const double value);
-	Vector3& operator/=(const double value);
+	Vector3& operator+=(const Vector3& other) noexcept;
+	Vector3& operator*=(const double value) noexcept;
+	Vector3& operator/=(const double value) noexcept;
 
-	Vector3 operator+(const Vector3& other) const;
-	Vector3 operator-(const Vector3& other) const;
-	Vector3 operator*(const Vector3& other) const;
-	Vector3 operator*(const double val) const;
-	Vector3 operator/(const double val) const;
+	Vector3 operator+(const Vector3& other) const noexcept;
+	Vector3 operator-(const Vector3& other) const noexcept;
+	Vector3 operator*(const Vector3& other) const noexcept;
+	Vector3 operator*(const double val) const noexcept;
+	Vector3 operator/(const double val) const noexcept;
 
-	double Length() const;
-	double SqrLength() const;
+	friend Vector3 operator*(const double val, const Vector3& vec) noexcept;
 
-	bool NearZero() const;
+	double Length() const noexcept;
+	double SqrLength() const noexcept;
 
-	static Vector3 Normalized(const Vector3& vec);
+	bool NearZero() const noexcept;
 
-	static double Dot(const Vector3& a, const Vector3& b);
-	static Vector3 Cross(const Vector3& a, const Vector3& b);
+	static Vector3 Normalized(const Vector3& vec) noexcept;
 
-	static Vector3 Reflect(const Vector3& vec, const Vector3& normal);
-	static Vector3 Refract(const Vector3& vec, const Vector3& normal, double etai_over_etat);
+	static double Dot(const Vector3& a, const Vector3& b) noexcept;
+	static Vector3 Cross(const Vector3& a, const Vector3& b) noexcept;
+
+	static Vector3 Reflect(const Vector3& vec, const Vector3& normal) noexcept;
+	static Vector3 Refract(const Vector3& vec, const Vector3& normal, const double etai_over_etat) noexcept;
 };
-
-
-// Non-member Vector3 operators
-inline std::ostream& operator<<(std::ostream& out, const Vector3& vec);
-inline Vector3 operator*(const double val, const Vector3& vec);
-
-
-// Type aliases for Vector3
-using Point3 = Vector3;			// 3D point
-using Color = Vector3;			// RGB color
